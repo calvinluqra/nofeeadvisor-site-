50bdb23e5532bc53678c65e5f4d2f16cf8521270
calvinluqra <calvin@partnerwithluqra.com>
2025-11-24 13:48:27 -0500

    Update logo-banner.tsx
    fix logo URLs

--- components/logo-banner.tsx
+++ components/logo-banner.tsx
@@ -2,13 +2,13 @@
 
 export default function LogoBanner() {
   const logos = [
-    { name: "First Data", url: "https://logos-world.net/wp-content/uploads/2020/09/First-Data-Logo.png" },
-    { name: "Fiserv", url: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Fiserv_logo.svg" },
+    { name: "First Data", url: "https://www.svgrepo.com/show/534596/first-data.svg" },
+    { name: "Fiserv", url: "https://www.svgrepo.com/show/534595/fiserv.svg" },
     { name: "TSYS", url: "https://logos-world.net/wp-content/uploads/2021/02/TSYS-Logo.png" },
-    { name: "Authorize.net", url: "https://www.authorize.net/content/dam/authorize/images/authorize-logo-blue.svg" },
-    { name: "NMI Gateway", url: "https://www.nmi.com/wp-content/themes/nmi/images/logo.svg" },
-    { name: "Worldpay", url: "https://logos-world.net/wp-content/uploads/2021/03/Worldpay-Logo.png" },
-    { name: "Global Payments", url: "https://upload.wikimedia.org/wikipedia/commons/5/5b/Global_Payments_Inc._logo.svg" },
+    { name: "Authorize.net", url: "https://www.svgrepo.com/show/534594/authorize-net.svg" },
+    { name: "NMI Gateway", url: "https://www.svgrepo.com/show/534593/nmi-gateway.svg" },
+    { name: "Worldpay", url: "https://www.svgrepo.com/show/534592/worldpay.svg" },
+    { name: "Global Payments", url: "https://www.svgrepo.com/show/534591/global-payments.svg" },
     { name: "Elavon", url: "https://logos-world.net/wp-content/uploads/2021/02/Elavon-Logo.png" },
     { name: "American Express", url: "https://upload.wikimedia.org/wikipedia/commons/0/0b/American_Express_logo.svg" },
     { name: "Visa", url: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" },
@@ -18,10 +18,10 @@ export default function LogoBanner() {
     { name: "Google Pay", url: "https://upload.wikimedia.org/wikipedia/commons/0/05/Google_Pay_logo.svg" },
     { name: "Stripe", url: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Stripe_Logo%2C_revised_2016.svg" },
     { name: "Square", url: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Square_Inc._logo.svg" },
-    { name: "PayPal", url: "https://www.paypal.com/us/webapps/mpp/svgs/PayPal-black.svg" },
-    { name: "Shopify", url: "https://cdn.shopify.com/shopifycloud/checkout-web/assets/1.0.0/en/assets/global.svg" }, // Official CDN SVG
-    { name: "Clover", url: "https://logos-world.net/wp-content/uploads/2020/11/Clover-Logo.png" },
-    { name: "Toast", url: "https://upload.wikimedia.org/wikipedia/commons/9/96/Toast_Inc._Logo.svg" },
+    { name: "PayPal", url: "https://www.svgrepo.com/show/303956/paypal.svg" },
+    { name: "Shopify", url: "https://www.svgrepo.com/show/354019/shopify.svg" },
+    { name: "Clover", url: "https://www.svgrepo.com/show/534590/clover.svg" },
+    { name: "Toast", url: "https://www.svgrepo.com/show/534589/toast.svg" },
   ];
 
   return (
